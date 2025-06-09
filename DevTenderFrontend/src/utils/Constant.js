95dbb05c0c7ddedea13067777e5fd5c27ed628c1
Adarsh kumar <adarshkumar0128@gmail.com>
2025-06-10 00:09:55 +0530

    .

--- DevTenderFrontend/src/utils/Constant.js
+++ DevTenderFrontend/src/utils/Constant.js
@@ -1,3 +1,4 @@
 // export const BASE_URL="/api"
 // export const BASE_URL="https://devtenderbackend.onrender.com"
-export const BASE_URL = "http://localhost:7777";
+
+export const BASE_URL = "https://devtinder-o90i.onrender.com";
