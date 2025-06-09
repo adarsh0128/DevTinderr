61dd3293bbc4024b72bc2e2b353ccb6051955a08
Adarsh kumar <adarshkumar0128@gmail.com>
2025-06-10 01:46:08 +0530

    some changes

--- devTenderBackend/src/utils/socket.js
+++ devTenderBackend/src/utils/socket.js
@@ -12,7 +12,7 @@ const getSecretRoomId = (userId, chatid) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://tinderfordeveloper.vercel.app",
     },
   });
 
